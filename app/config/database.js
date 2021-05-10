66169a87a6ec1c2e9fde124b0c53394978c60d0c
rafaooliveira <raafasantos1@gmail.com>
2021-05-10 14:52:44 -0300

    database

--- app/config/database.js
+++ app/config/database.js
@@ -5,15 +5,19 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'postgres'),
+        host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
         schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
+      },
+      options: {
         ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {}
     },
   },
 });
